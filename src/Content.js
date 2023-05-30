4e325c672717244f1eab675731ac0c542164569a
haszanakauna <haszankauna@gmail.com>
2023-05-30 03:31:42 +0100

    added a second button

--- src/Content.js
+++ src/Content.js
@@ -4,10 +4,14 @@ function Content() {
     const handleClick = () => {
         console.log('You clicked it')
     }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
   return (
     <main>
       <p>Hello</p>
       <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2('Click')}>Click It</button>
     </main>
   )
 }
