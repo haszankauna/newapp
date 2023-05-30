6bd693c849491f422d815fd25b81e4d862403b38
haszanakauna <haszankauna@gmail.com>
2023-05-30 04:05:39 +0100

    added icon

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
 
 function Content() {
     const [items, setItems] = useState([
@@ -21,7 +22,21 @@ function Content() {
     ]);
   return (
     <main>
-
+        <ul>
+            {items.map((item) => (
+                <li className='item' key={item.id}>
+                    <input
+                    type='checkbox'
+                    checked={item.checked}
+                    />
+                    <label>{item.item}</label>
+                    <FaTrashAlt
+                    role='button' 
+                    tabIndex='0'
+                    />
+                </li>
+            ))}
+        </ul>
     </main>
   )
 }
