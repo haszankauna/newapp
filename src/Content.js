8be3837ff1f9c6c82f6b27f4b531cfcbc8bc840d
haszanakauna <haszankauna@gmail.com>
2023-05-30 03:25:47 +0100

    added a button

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Content() {
+    const handleClick = () => {
+        console.log('You clicked it')
+    }
   return (
     <main>
       <p>Hello</p>
+      <button onClick={handleClick}>Click It</button>
     </main>
   )
 }
